73412bdae15dbec704fb7d63f7df43b955bc8a8b
Kaio Giorgetti <kaiomacielgiorgetti@gmail.com>
2024-12-19 09:05:52 -0300

    Add files via upload

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,22 @@
+import Task from "./Task"
+
+const TaskList = ({tasks, onDeleteTask, onToggleTaskDone}) => {
+
+    if(tasks.length === 0) {
+        return <p>Não há tarefas para serem feitas</p>
+    }
+
+  return (
+    <ul>
+    {tasks.map((task) => (
+        <Task 
+            key={task.id} 
+            task={task} 
+            onDelete={() => onDeleteTask(task.id)} 
+            onToggleDone={() => onToggleTaskDone(task.id)}/>
+    ))}
+    </ul>
+  )
+}
+
+export default TaskList
